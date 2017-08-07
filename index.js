22f4357498329936e5720f174dece6ed9082d9d1
Marc-Antoine Dubé <madube94@gmail.com>
2017-08-07 11:36:23 -0400

    [FEAT] Add circle icons

---
+++ index.js
@@ -0,0 +1,12 @@
+// Circle icons
+export let authenticationCircle = require("./assets/icons-circle/authentication-circle.svg");
+export let cloudConsoleCircle = require("./assets/icons-circle/cloud-console-circle.svg");
+export let cloudStorageCircle = require("./assets/icons-circle/cloud-storage-circle.svg");
+export let fullTextSearchCircle = require("./assets/icons-circle/full-text-search-circle.svg");
+export let languageAgnosticCircle = require("./assets/icons-circle/language-agnostic-circle.svg");
+export let payAsYouGoCircle = require("./assets/icons-circle/pay-as-you-go-circle.svg");
+export let pushNotificationCircle = require("./assets/icons-circle/push-notification-circle.svg");
+export let restApiCircle = require("./assets/icons-circle/rest-api-circle.svg");
+export let scaleCircle = require("./assets/icons-circle/scale-circle.svg");
+export let secureCircle = require("./assets/icons-circle/secure-circle.svg");
+export let teamCircle = require("./assets/icons-circle/team-circle.svg");
