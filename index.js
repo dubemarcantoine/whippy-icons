6a78695c487a2b9f5a3263fac7ea6f9377297935
Marc-Antoine Dubé <madube94@gmail.com>
2017-08-07 21:18:28 -0400

    [FIX] Fix declarations and exports

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ export const scale = require("./assets/icons/scale.svg");
 export const secure = require("./assets/icons/secure.svg");
 export const team = require("./assets/icons/team.svg");
 
-//  icons
-export const authentication = require("./assets/icons/authentication.svg");
-export const cloudConsole = require("./assets/icons/cloud-console.svg");
-export const cloudStorage = require("./assets/icons/cloud-storage.svg");
-export const fullTextSearch = require("./assets/icons/full-text-search.svg");
-export const languageAgnostic = require("./assets/icons/language-agnostic.svg");
-export const liveDatabase = require("./assets/icons/live-database.svg");
-export const payAsYouGo = require("./assets/icons/pay-as-you-go.svg");
-export const pushNotification = require("./assets/icons/push-notification.svg");
-export const restApi = require("./assets/icons/rest-api.svg");
-export const scale = require("./assets/icons/scale.svg");
-export const secure = require("./assets/icons/secure.svg");
-export const team = require("./assets/icons/team.svg");
+// Circle icons
+export const authenticationCircle = require("./assets/icons-circle/authentication-circle.svg");
+export const cloudConsoleCircle = require("./assets/icons-circle/cloud-console-circle.svg");
+export const cloudStorageCircle = require("./assets/icons-circle/cloud-storage-circle.svg");
+export const fullTextSearchCircle = require("./assets/icons-circle/full-text-search-circle.svg");
+export const languageAgnosticCircle = require("./assets/icons-circle/language-agnostic-circle.svg");
+export const liveDatabaseCircle = require("./assets/icons-circle/live-database-circle.svg");
+export const payAsYouGoCircle = require("./assets/icons-circle/pay-as-you-go-circle.svg");
+export const pushNotificationCircle = require("./assets/icons-circle/push-notification-circle.svg");
+export const restApiCircle = require("./assets/icons-circle/rest-api-circle.svg");
+export const scaleCircle = require("./assets/icons-circle/scale-circle.svg");
+export const secureCircle = require("./assets/icons-circle/secure-circle.svg");
+export const teamCircle = require("./assets/icons-circle/team-circle.svg");
