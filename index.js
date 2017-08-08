159d5e4e8d34abf0c0d07bcadad5af8b7d1a415d
Marc-Antoine Dubé <madube94@gmail.com>
2017-08-07 20:28:03 -0400

    [FEAT] Add icons

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-// Circle icons
-export let authenticationCircle = require("./assets/icons-circle/authentication-circle.svg");
-export let cloudConsoleCircle = require("./assets/icons-circle/cloud-console-circle.svg");
-export let cloudStorageCircle = require("./assets/icons-circle/cloud-storage-circle.svg");
-export let fullTextSearchCircle = require("./assets/icons-circle/full-text-search-circle.svg");
-export let languageAgnosticCircle = require("./assets/icons-circle/language-agnostic-circle.svg");
-export let liveDatabase = require("./assets/icons-circle/live-database-circle.svg");
-export let payAsYouGoCircle = require("./assets/icons-circle/pay-as-you-go-circle.svg");
-export let pushNotificationCircle = require("./assets/icons-circle/push-notification-circle.svg");
-export let restApiCircle = require("./assets/icons-circle/rest-api-circle.svg");
-export let scaleCircle = require("./assets/icons-circle/scale-circle.svg");
-export let secureCircle = require("./assets/icons-circle/secure-circle.svg");
-export let teamCircle = require("./assets/icons-circle/team-circle.svg");
+//  icons
+export let authentication = require("./assets/icons/authentication.svg");
+export let cloudConsole = require("./assets/icons/cloud-console.svg");
+export let cloudStorage = require("./assets/icons/cloud-storage.svg");
+export let fullTextSearch = require("./assets/icons/full-text-search.svg");
+export let languageAgnostic = require("./assets/icons/language-agnostic.svg");
+export let liveDatabase = require("./assets/icons/live-database.svg");
+export let payAsYouGo = require("./assets/icons/pay-as-you-go.svg");
+export let pushNotification = require("./assets/icons/push-notification.svg");
+export let restApi = require("./assets/icons/rest-api.svg");
+export let scale = require("./assets/icons/scale.svg");
+export let secure = require("./assets/icons/secure.svg");
+export let team = require("./assets/icons/team.svg");
+
+//  icons
+export let authentication = require("./assets/icons/authentication.svg");
+export let cloudConsole = require("./assets/icons/cloud-console.svg");
+export let cloudStorage = require("./assets/icons/cloud-storage.svg");
+export let fullTextSearch = require("./assets/icons/full-text-search.svg");
+export let languageAgnostic = require("./assets/icons/language-agnostic.svg");
+export let liveDatabase = require("./assets/icons/live-database.svg");
+export let payAsYouGo = require("./assets/icons/pay-as-you-go.svg");
+export let pushNotification = require("./assets/icons/push-notification.svg");
+export let restApi = require("./assets/icons/rest-api.svg");
+export let scale = require("./assets/icons/scale.svg");
+export let secure = require("./assets/icons/secure.svg");
+export let team = require("./assets/icons/team.svg");
